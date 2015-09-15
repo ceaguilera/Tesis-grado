5f4edc61e5bfae8e8f874b1ecc61ada945ce385a
Carlos <ceaf.21@gmail.com>
2015-09-14 23:52:04 -0430

    %Creado el layout y footer para manejar en block las vistas +agregados las vistas y todos los controladores de este modulo

--- src/Tati/ProcesoBundle/Controller/DefaultController.php
+++ src/Tati/ProcesoBundle/Controller/DefaultController.php
@@ -6,8 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function pruebaAction()
+    public function welcomeAction()
     {
-        return $this->render('ProcesoBundle:Default:index.html.twig');
+        return $this->render('ProcesoBundle:All:welcome.html.twig');
+    }
+    public function addProcessAction()
+    {
+        return $this->render('ProcesoBundle:All:addprocess.html.twig');
+    }
+    public function listProcessActiveAction()
+    {
+        return $this->render('ProcesoBundle:All:listProcessAct.html.twig');
+    }
+    public function listProcessInactiveAction()
+    {
+        return $this->render('ProcesoBundle:All:listProcessInactive.html.twig');
+    }
+    public function listTaskAction()
+    {
+        return $this->render('ProcesoBundle:All:listTask.html.twig');
+    }
+    public function listRolesAction()
+    {
+        return $this->render('ProcesoBundle:All:listRoles.html.twig');
     }
 }
