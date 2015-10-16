6a94bc330a7468a58ae7885d75044987065e3ae6
Carlos Aguilera <ceaf.21@gmail.com>
2015-10-16 16:49:13 -0430

    %corregido el error del datafixture

--- src/Tati/ProcesoBundle/DataFixtures/ORM/LoadTareaData.php
+++ src/Tati/ProcesoBundle/DataFixtures/ORM/LoadTareaData.php
@@ -5,20 +5,24 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Cupon\OfertaBundle\Entity\Tarea;
+use Tati\ProcesoBundle\Entity\Tarea as ETarea;
 
-class Tarea extends AbstractFixture implements OrderedFixtureInterface
+class LoadTareaData extends AbstractFixture implements OrderedFixtureInterface
 {
    
     public function load(ObjectManager $manager)
     {
 
         //Carga de datos de una tarea nueva
-        $Tarea = new Tarea();
+        $Tarea = new ETarea();
         $Tarea->setNombre('Tarea1');
+        $Tarea->setSlug('Tarea1');
         $Tarea->setDescripcion('esta tarea se encarga de una cosa tal');
         $manager->persist($Tarea);
         $manager->flush();
     }
-    
+    public function getOrder()
+    {
+        return 1;
+    }
 }
